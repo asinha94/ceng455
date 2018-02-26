bfad63dc5a1b4ed1ebcd072f3178caaae0282780
Anuraag Sinha <asinha@uvic.ca>
2018-02-26 01:18:51 -0800

    adding new stuff

---
+++ lab2/serial_echo/Debug/Generated_Code/pin_init.d
@@ -0,0 +1,46 @@
+Generated_Code/pin_init.o: ../Generated_Code/pin_init.c \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/fsl_device_registers.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cm4.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmInstr.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmFunc.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmSimd.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/Generated_Code/SDK/platform/devices/MK64F12/startup/system_MK64F12.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12_extension.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/fsl_bitaccess.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12_features.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/hal/inc/fsl_port_hal.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/hal/inc/fsl_sim_hal.h \
+ c:\users\asinha\documents\workspace\serial_echo\sdk\platform\hal\src\sim\mk64f12\fsl_sim_hal_mk64f12.h \
+ ../Generated_Code/pin_init.h
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/fsl_device_registers.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cm4.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/Generated_Code/SDK/platform/devices/MK64F12/startup/system_MK64F12.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12_extension.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/fsl_bitaccess.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12_features.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/hal/inc/fsl_port_hal.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/hal/inc/fsl_sim_hal.h:
+
+c:\users\asinha\documents\workspace\serial_echo\sdk\platform\hal\src\sim\mk64f12\fsl_sim_hal_mk64f12.h:
+
+../Generated_Code/pin_init.h:
---
+++ lab2/serial_echo/Debug/SDK/platform/hal/src/sim/MK64F12/fsl_sim_hal_MK64F12.d
@@ -0,0 +1,41 @@
+SDK/platform/hal/src/sim/MK64F12/fsl_sim_hal_MK64F12.o: \
+ ../SDK/platform/hal/src/sim/MK64F12/fsl_sim_hal_MK64F12.c \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/fsl_device_registers.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cm4.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmInstr.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmFunc.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmSimd.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/Generated_Code/SDK/platform/devices/MK64F12/startup/system_MK64F12.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12_extension.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/fsl_bitaccess.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12_features.h \
+ C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/hal/inc/fsl_sim_hal.h \
+ c:\users\asinha\documents\workspace\serial_echo\sdk\platform\hal\src\sim\mk64f12\fsl_sim_hal_mk64f12.h
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/fsl_device_registers.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cm4.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/Generated_Code/SDK/platform/devices/MK64F12/startup/system_MK64F12.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12_extension.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/fsl_bitaccess.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/devices/MK64F12/include/MK64F12_features.h:
+
+C:/Users/asinha/Documents/workspace/serial_echo/SDK/platform/hal/inc/fsl_sim_hal.h:
+
+c:\users\asinha\documents\workspace\serial_echo\sdk\platform\hal\src\sim\mk64f12\fsl_sim_hal_mk64f12.h:
